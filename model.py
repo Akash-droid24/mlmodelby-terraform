a29a47b8ff29ebc35fcac1ea66ed86f0674ada12
Akash-droid24 <64472357+Akash-droid24@users.noreply.github.com>
2021-05-27 20:04:47 +0530

    test

--- model.py
+++ model.py
@@ -12,17 +12,37 @@ x = x.values
 x = x.reshape(30,1)
 
 print("                THE MODEL IS BEING TRAINED          ")
+print("\n")
+print("\n")
 from sklearn.linear_model import LinearRegression
 mind = LinearRegression()
 
 # Training the model
 mind.fit( x , y)
-print("******MODEL TRAINED SUCCESSFULLY")
+print("\t\t\t\t\tMODEL TRAINED SUCCESSFULLY")
 print("\n")
 value_to_predict = eval(input("Enter the value to predict: "))
-
-print(mind.predict( [[value_to_predict]] ))
+print("\n")
+print("The predicted value of salary is ",mind.predict( [[value_to_predict]] ))
 print("\n")
 
 print("Machine Learning model created")
+print("\n\n")
+
+print("""
+      You want to save the Machine learning model :
+           Press Y for yes 
+           Press N for no
+      """)
+
+choice = input()
+
+if "Y" in choice:
+  import joblib
+  joblib.dump(model, 'model1.pk1')
+  print("Your model has been saved")
+  
+if "N" in choice:
+  break
+  
 
